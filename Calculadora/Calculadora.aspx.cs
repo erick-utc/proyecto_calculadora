7782ffb4c1014685c7416966bbe24e3f5b7ca3d5
AlonsoVS21 <viquezalonso7@gmail.com>
2023-11-05 10:05:06 -0600

    Funcionalidad Numeros y botones

--- Calculadora/Calculadora.aspx.cs
+++ Calculadora/Calculadora.aspx.cs
@@ -9,10 +9,21 @@ namespace Calculadora
 {
     public partial class Calculadora : System.Web.UI.Page
     {
+           
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void agregarNumero(string numero)
+        {
+            if (txtNumero1.Text == "0")
+                txtNumero1.Text = "";
+            txtNumero1.Text += numero;
+
+
+        }
+
 
         protected void btnSuma_Click(object sender, EventArgs e)
         {
@@ -21,7 +32,102 @@ namespace Calculadora
 
         protected void btnCero_Click(object sender, EventArgs e)
         {
-            Console.WriteLine(0);
+
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+
+        }
+
+        protected void btnUno_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnDos_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnTres_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnCuatro_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnCinco_Click(object sender, EventArgs e)
+        {
+
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnSeis_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnSiete_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnOcho_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void btnNueve_Click(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            agregarNumero(boton.Text);
+        }
+
+        protected void bntDelete_Click(object sender, EventArgs e)
+        {
+            if (txtNumero1.Text.Length > 1)
+            {
+                txtNumero1.Text = txtNumero1.Text.Substring(0, txtNumero1.Text.Length - 1);
+
+
+
+
+            }
+            else
+            {
+                txtNumero1.Text = "0";
+
+
+
+            }
+
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = "0";
+        }
+
+        protected void btnPunto_Click(object sender, EventArgs e)
+        {
+            if (!txtNumero1.Text.Contains("."))
+            {
+                txtNumero1.Text += ".";
+
+
+
+            }
         }
     }
 }
